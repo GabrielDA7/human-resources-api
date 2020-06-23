999259a084086ebad48e7b8b64043919f7ce4870
sybernatuz <decultot.louis@gmail.com>
2020-06-23 12:07:47 +0200

    init behat

--- api/tests/Behat/Context/ApiFeatureContext.php
+++ api/tests/Behat/Context/ApiFeatureContext.php
@@ -17,7 +17,7 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-require_once __DIR__  . '/../../../bin/.phpunit/phpunit-8.3-0/src/Framework/Assert/Functions.php';
+require_once __DIR__  . '/../../../bin/.phpunit/phpunit-7.5-0/src/Framework/Assert/Functions.php';
 
 class ApiFeatureContext implements Context
 {
